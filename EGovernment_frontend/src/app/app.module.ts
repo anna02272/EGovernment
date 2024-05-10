4bc23acc5e850e4ba5154e5c695f884f008cb54e
cukelli <anastasija.cukelj@gmail.com>
2024-05-11 00:18:19 +0200

    login and register templates

--- EGovernment_frontend/src/app/app.module.ts
+++ EGovernment_frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -8,12 +7,16 @@ import { HomeComponent } from './components/home/home.component';
 import { ConfigService } from './services/config.service';
 import { ApiService } from './services/api.service';
 import { RefreshService } from './services/refresh.service';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,

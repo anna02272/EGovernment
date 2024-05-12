19b8a1e22481cb86e32d0d6c8c78445184f82a6d
Ana Domonji <96575598+anna02272@users.noreply.github.com>
2024-05-12 22:20:34 +0200

    Traffic police (#6)
    Co-authored-by: flower0408 <natasakotaranin1@gmail.com>

---
+++ EGovernment_backend/police-service/domain/delict.go
@@ -0,0 +1,43 @@
+package domain
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Delict struct {
+	ID                         primitive.ObjectID `bson:"_id" json:"id"`
+	PolicemanID                string             `bson:"policeman_id" json:"policeman_id"`
+	DriverIdentificationNumber string             `bson:"driver_identification_number" json:"driver_identification_number"`
+	VehicleLicenceNumber       string             `bson:"vehicle_licence_number" json:"vehicle_licence_number"`
+	DriverEmail                string             `bson:"driver_email" json:"driver_email"`
+	Date                       primitive.DateTime `bson:"date" json:"date"`
+	Location                   string             `bson:"location" json:"location"`
+	Description                string             `bson:"description" json:"description"`
+	DelictType                 DelictType         `bson:"delict_type" json:"delict_type"`
+	NumberOfPenaltyPoints      int64              `bson:"number_of_penalty_points" json:"number_of_penalty_points"`
+}
+
+type DelictType string
+
+const (
+	Speeding                                  = "Speeding"
+	DrivingUnderTheInfluence                  = "DrivingUnderTheInfluence"
+	ImproperOvertaking                        = "ImproperOvertaking"
+	ImproperParking                           = "ImproperParking"
+	FailureTooComplyWithTrafficLightsAndSigns = "FailureTooComplyWithTrafficLightsAndSigns"
+	ImproperUseOfSeatBeltsAndChildSeats       = "ImproperUseOfSeatBeltsAndChildSeats"
+	UsingMobilePhoneWhileDriving              = "UsingMobilePhoneWhileDriving"
+	ImproperUseOfMotorVehicle                 = "ImproperUseOfMotorVehicle"
+)
+
+type DelictCreate struct {
+	PolicemanID                string             `bson:"policeman_id" json:"policeman_id"`
+	DriverIdentificationNumber string             `bson:"driver_identification_number" json:"driver_identification_number"`
+	VehicleLicenceNumber       string             `bson:"vehicle_licence_number" json:"vehicle_licence_number"`
+	DriverEmail                string             `bson:"driver_email" json:"driver_email"`
+	Date                       primitive.DateTime `bson:"date" json:"date"`
+	Location                   string             `bson:"location" json:"location"`
+	Description                string             `bson:"description" json:"description"`
+	DelictType                 DelictType         `bson:"delict_type" json:"delict_type"`
+	NumberOfPenaltyPoints      int64              `bson:"number_of_penalty_points" json:"number_of_penalty_points"`
+}
